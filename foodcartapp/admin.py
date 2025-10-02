98bfe6c7f6d1e7180d2627ac4373e87a064be28e
AlexKlos <klos@list.ru>
2025-10-02 19:52:50 +0700

    Initial commit

---
+++ foodcartapp/admin.py
@@ -0,0 +1,131 @@
+from django.contrib import admin
+from django.shortcuts import reverse
+from django.templatetags.static import static
+from django.utils.html import format_html
+
+from .models import Product
+from .models import ProductCategory
+from .models import Restaurant
+from .models import RestaurantMenuItem
+from .models import Order, OrderItems
+
+
+class RestaurantMenuItemInline(admin.TabularInline):
+    model = RestaurantMenuItem
+    extra = 0
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    search_fields = [
+        'name',
+        'address',
+        'contact_phone',
+    ]
+    list_display = [
+        'name',
+        'address',
+        'contact_phone',
+    ]
+    inlines = [
+        RestaurantMenuItemInline
+    ]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'get_image_list_preview',
+        'name',
+        'category',
+        'price',
+    ]
+    list_display_links = [
+        'name',
+    ]
+    list_filter = [
+        'category',
+    ]
+    search_fields = [
+        # FIXME SQLite can not convert letter case for cyrillic words properly, so search will be buggy.
+        # Migration to PostgreSQL is necessary
+        'name',
+        'category__name',
+    ]
+
+    inlines = [
+        RestaurantMenuItemInline
+    ]
+    fieldsets = (
+        ('Общее', {
+            'fields': [
+                'name',
+                'category',
+                'image',
+                'get_image_preview',
+                'price',
+            ]
+        }),
+        ('Подробно', {
+            'fields': [
+                'special_status',
+                'description',
+            ],
+            'classes': [
+                'wide'
+            ],
+        }),
+    )
+
+    readonly_fields = [
+        'get_image_preview',
+    ]
+
+    class Media:
+        css = {
+            "all": (
+                static("admin/foodcartapp.css")
+            )
+        }
+
+    def get_image_preview(self, obj):
+        if not obj.image:
+            return 'выберите картинку'
+        return format_html('<img src="{url}" style="max-height: 200px;"/>', url=obj.image.url)
+    get_image_preview.short_description = 'превью'
+
+    def get_image_list_preview(self, obj):
+        if not obj.image or not obj.id:
+            return 'нет картинки'
+        edit_url = reverse('admin:foodcartapp_product_change', args=(obj.id,))
+        return format_html('<a href="{edit_url}"><img src="{src}" style="max-height: 50px;"/></a>', edit_url=edit_url, src=obj.image.url)
+    get_image_list_preview.short_description = 'превью'
+
+
+@admin.register(ProductCategory)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+class OrderItemsInline(admin.TabularInline):
+    model = OrderItems
+    extra = 0
+    raw_id_fields = ('product',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'firstname', 'lastname', 'phonenumber', 'address', 'items_count')
+    search_fields = ('id', 'firstname', 'lastname', 'phonenumber', 'address')
+    inlines = [OrderItemsInline]
+
+    def items_count(self, obj):
+        return obj.items.count()
+    items_count.short_description = 'Позиций'
+
+
+@admin.register(OrderItems)
+class OrderItemsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'product', 'quantity')
+    raw_id_fields = ('order', 'product')
+    search_fields = ('order__id', 'product__name')
---
+++ foodcartapp/views.py
@@ -0,0 +1,83 @@
+import json
+
+from django.http import JsonResponse
+from django.templatetags.static import static
+
+
+from .models import Product, Order, OrderItems
+
+
+def banners_list_api(request):
+    # FIXME move data to db?
+    return JsonResponse([
+        {
+            'title': 'Burger',
+            'src': static('burger.jpg'),
+            'text': 'Tasty Burger at your door step',
+        },
+        {
+            'title': 'Spices',
+            'src': static('food.jpg'),
+            'text': 'All Cuisines',
+        },
+        {
+            'title': 'New York',
+            'src': static('tasty.jpg'),
+            'text': 'Food is incomplete without a tasty dessert',
+        }
+    ], safe=False, json_dumps_params={
+        'ensure_ascii': False,
+        'indent': 4,
+    })
+
+
+def product_list_api(request):
+    products = Product.objects.select_related('category').available()
+
+    dumped_products = []
+    for product in products:
+        dumped_product = {
+            'id': product.id,
+            'name': product.name,
+            'price': product.price,
+            'special_status': product.special_status,
+            'description': product.description,
+            'category': {
+                'id': product.category.id,
+                'name': product.category.name,
+            } if product.category else None,
+            'image': product.image.url,
+            'restaurant': {
+                'id': product.id,
+                'name': product.name,
+            }
+        }
+        dumped_products.append(dumped_product)
+    return JsonResponse(dumped_products, safe=False, json_dumps_params={
+        'ensure_ascii': False,
+        'indent': 4,
+    })
+
+
+def register_order(request):
+    try:
+        order_data = json.loads(request.body.decode('utf-8'))
+    except ValueError as e:
+        return JsonResponse({'error': e})
+    
+    order = Order.objects.create(
+        firstname=order_data['firstname'],
+        lastname=order_data['lastname'],
+        phonenumber=order_data['phonenumber'],
+        address=order_data['address'],
+    )
+
+    for item in order_data['products']:
+        product = Product.objects.get(id=item['product'])
+        OrderItems.objects.create(
+            order=order,
+            product=product,
+            quantity=item['quantity'],
+        )
+
+    return JsonResponse({'order_id': order.id})
