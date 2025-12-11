cde4977dd300c9ecc805c482249b1b75472886c2
AlexKlos <klos@list.ru>
2025-12-11 09:21:49 +0700

    Улучшение - Не используйте индекс для уникального поля

--- locations/models.py
+++ locations/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Location(models.Model):
-    address = models.CharField('адрес', max_length=255, unique=True, db_index=True)
+    address = models.CharField('адрес', max_length=200, unique=True)
     lon = models.FloatField('долгота', null=True, blank=True)
     lat = models.FloatField('широта', null=True, blank=True)
     updated_at = models.DateTimeField('обновлено', auto_now=True)
